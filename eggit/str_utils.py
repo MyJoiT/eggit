013cb0efe95762c88aba258f5d8fdae6f8298548
JoiT <1076869692@qq.com>
2017-12-27 21:07:08 +0800

    modify

--- eggit/str_utils.py
+++ eggit/str_utils.py
@@ -3,5 +3,5 @@ class DTFormat(object):
     datetime_format = None
 
     def __init__(self, date_format='%Y-%m-%d', datetime_format='%Y-%m-%d %H:%M:%S'):
-        self.date_format = self.date_format
-        self.datetime_format = self.datetime_format
+        self.date_format = date_format
+        self.datetime_format = datetime_format
