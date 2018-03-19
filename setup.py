1494f5767b3548b825c275c88c792e17b7b2ec82
JoiT <myjoit@outlook.com>
2018-03-19 03:58:49 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='eggit',
     version='0.0.1',
     description='a python lib',
-    auther='JoiT',
+    author='JoiT',
     author_email='myjoit@outlook.com',
     url='https://github.com/MyJoiT/eggit',
     download_url='https://github.com/MyJoiT/eggit/archive/0.0.1.tar.gz',
     packages=find_packages(exclude=[]),
     keywords=('eggit'),
     install_requires=[],
-    zip_save=False
+    zip_safe=False
 )
