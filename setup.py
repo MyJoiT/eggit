e4a2a53f5272f57acae546b3836401e2b25f3382
JoiT <myjoit@outlook.com>
2018-07-01 21:15:05 +0800

    add pyjwt-monkey

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='eggit',
-    version='0.0.2',
+    version='0.0.3',
     description='a python lib',
     author='JoiT',
     author_email='myjoit@outlook.com',
     url='https://github.com/MyJoiT/eggit',
-    download_url='https://github.com/MyJoiT/eggit/archive/0.0.2.tar.gz',
+    download_url='https://github.com/MyJoiT/eggit/archive/0.0.3.tar.gz',
     packages=find_packages(exclude=[]),
     keywords=('eggit, lib, tool'),
-    install_requires=[],
+    install_requires=[
+        'pyjwt>=1.4.2'
+        ],
     license='GPL3',
     zip_safe=True
 )
