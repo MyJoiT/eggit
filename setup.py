7f83b8545f985d465e2399825864c988f4be59a1
JoiT <myjoit@outlook.com>
2018-07-03 13:41:07 +0800

    add some tools

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='eggit',
-    version='0.0.5',
+    version='0.0.2',
     description='a python lib',
     author='JoiT',
     author_email='myjoit@outlook.com',
     url='https://github.com/MyJoiT/eggit',
-    download_url='https://github.com/MyJoiT/eggit/archive/0.0.5.tar.gz',
+    download_url='https://github.com/MyJoiT/eggit/archive/0.0.2.tar.gz',
     packages=find_packages(exclude=[]),
     keywords=('eggit, lib, tool'),
     install_requires=[
-        'pyjwt>=1.4.2'
+        'pyjwt>=1.4.2',
+        'sqlalchemy>=1.2.7'
         ],
     license='GPL3',
     zip_safe=True
