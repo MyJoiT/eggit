ea4fd4e34233fa607daf50e89490d1ad68532760
JoiT <myjoit@outlook.com>
2018-07-04 14:33:57 +0800

    add long_description

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a python lib for all programmers !
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='eggit',
     version='0.0.9',
     description='a python lib',
+    long_description=long_description,
     author='JoiT',
     author_email='myjoit@outlook.com',
     url='https://github.com/MyJoiT/eggit',
