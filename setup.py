7d1c2bae95033d0945cbdce10d9d3edbd5ae9152
JoiT <myjoit@outlook.com>
2018-07-10 15:52:29 +0800

    flask-restful Api fixed

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='eggit',
-    version='0.0.9',
+    version='0.0.10',
     description='a python lib',
     long_description=long_description,
     author='JoiT',
     author_email='myjoit@outlook.com',
     url='https://github.com/MyJoiT/eggit',
-    download_url='https://github.com/MyJoiT/eggit/archive/0.0.9.tar.gz',
+    download_url='https://github.com/MyJoiT/eggit/archive/0.0.10.tar.gz',
     packages=find_packages(exclude=[]),
     keywords=('eggit, lib, tool'),
     install_requires=[
         'pyjwt>=1.4.2',
-        'sqlalchemy>=1.2.7'
+        'sqlalchemy>=1.2.7',
+        'flask-restful>=0.3.6'
         ],
     license='GPL3',
     zip_safe=True
