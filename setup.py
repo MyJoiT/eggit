13e6aa8d60929102797f10f739fa0bbb89bba274
JoiT <myjoit@outlook.com>
2018-09-21 19:15:03 +0800

    update version number

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='eggit',
-    version='0.0.21',
+    version='0.0.22',
     description='a python lib',
     long_description=long_description,
     author='JoiT',
     author_email='myjoit@outlook.com',
     url='https://github.com/MyJoiT/eggit',
-    download_url='https://github.com/MyJoiT/eggit/archive/0.0.21.tar.gz',
+    download_url='https://github.com/MyJoiT/eggit/archive/0.0.22.tar.gz',
     packages=find_packages(exclude=[]),
     keywords=('eggit, lib, tool'),
     install_requires=[
