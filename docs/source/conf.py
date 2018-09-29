6ae4d02288404ba650323f3616d769981d2d77a7
JoiT <myjoit@outlook.com>
2018-09-29 20:36:23 +0800

    fixed doc bugs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,10 +16,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# import os
-# import sys
+import os
+import sys
 # 
-# sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
